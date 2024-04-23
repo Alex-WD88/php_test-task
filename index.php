12490024efef00c690f27690cb1573e27f076cfe
alex <dobryanskiy.aleksandr@gmail.com>
2024-04-23 21:58:49 +0300

    phone number

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
-<?php
-
-phpinfo();
+<form action="process.php" method="post">
+    <input type="text" name="phoneNumber" placeholder="Введите номер телефона">
+    <input type="submit" value="Определить страну">
+</form>
